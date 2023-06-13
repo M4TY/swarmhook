9235dc1bdf64e98e358a4244546bef531251472d
Matyáš Himmer <matyhimmer@gmail.com>
2023-06-13 08:32:45 +0200

    feat: Registry login

--- src/index.ts
+++ src/index.ts
@@ -78,30 +78,38 @@ const main = () => {
 }
 
 function exec(service: Service, version: string) {
-    const service_name = service.service_name;
-    if (version === "latest") {
-        const command = `docker service update ${service_name} --with-registry-auth`
-        child_process.exec(command, (error, stdout, stderr) => {
-            if (error) {
-                notify(`Error deploying ${service_name} to latest version`, NotificationType.error);
-                console.log(error);
-                return;
-            }
-            notify(`Successfully deployed ${service_name} to latest version`, NotificationType.success);
-            console.log(stdout);
-        });
-    } else {
-        const image = service.image + ":" + version;
-        const command = `docker service update ${service_name} --with-registry-auth --image ${image}`
-        child_process.exec(command, (error, stdout, stderr) => {
-            if (error) {
-                notify(`Error deploying ${service_name} to version ${version}`, NotificationType.error);
-                return;
-            }
-            notify(`Successfully deployed ${service_name} to version ${version}`, NotificationType.success);
-            console.log(stdout);
-        });
-    }
+    child_process.exec(`docker login -u ${process.env.DOCKER_USERNAME} -p ${process.env.DOCKER_PASSWORD}`, (error, stdout, stderr) => {
+        if (error) {
+            notify(`Error logging into docker registry`, NotificationType.error);
+            console.log(error);
+            return;
+        }
+
+        const service_name = service.service_name;
+        if (version === "latest") {
+            const command = `docker service update ${service_name} --with-registry-auth`
+            child_process.exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    notify(`Error deploying ${service_name} to latest version`, NotificationType.error);
+                    console.log(error);
+                    return;
+                }
+                notify(`Successfully deployed ${service_name} to latest version`, NotificationType.success);
+            });
+        } else {
+            const image = service.image + ":" + version;
+            const command = `docker service update ${service_name} --with-registry-auth --image ${image}`
+            child_process.exec(command,
+                (error, stdout, stderr) => {
+                    if (error) {
+                        notify(`Failed to deploy ${service_name} to version ${version}`, NotificationType.error);
+                        return;
+                    }
+                    notify(`Successfully deployed ${service_name} to version ${version}`, NotificationType.success);
+                });
+        }
+
+    });
 }
 
 const loadConfig = (): Config => {
